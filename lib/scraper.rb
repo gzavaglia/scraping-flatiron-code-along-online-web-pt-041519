d846b3ee7b0b020fa6e4eb9a4f0b876ec0b4839e
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-27 22:06:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,17 +5,20 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
-      end
-    end
-  end
+      end #end if
+    end #end do 
+  end #end print_courses method 
   
+  def get_page
+    
+  end
 end
 
 
